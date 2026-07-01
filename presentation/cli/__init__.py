297160f124de3155ea2915f875431dec94d6c771
jianming.luo <jianming.luo@shopee.com>
2026-07-01 11:43:55 +0800

    feat: add CLI portfolio display module

--- presentation/cli/__init__.py
+++ presentation/cli/__init__.py
@@ -1,5 +1,13 @@
 from .signal import render_signals
 from .etf import render_etf_detail
+from .portfolio import render_account_summary, render_holdings, render_recent_trades
 from .render import console
 
-__all__ = ["render_signals", "render_etf_detail", "console"]
+__all__ = [
+    "render_signals",
+    "render_etf_detail",
+    "console",
+    "render_account_summary",
+    "render_holdings",
+    "render_recent_trades",
+]
